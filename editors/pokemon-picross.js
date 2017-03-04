6c54b5e3b78956ee3d3dd49eb208b25bef0d1a02
Marc Robledo <mark@tinet.org>
2017-03-04 20:24:47 +0100

    added pokemon picross bought picrolites value, can unlock free shop now

--- editors/pokemon-picross.js
+++ editors/pokemon-picross.js
@@ -1,6 +1,6 @@
 /*
-	Pokémon Picross for HTML5 Save Editor v20160704
-	by Marc Robledo 2016
+	Pokémon Picross for HTML5 Save Editor v20170304
+	by Marc Robledo 2017
 */
 
 SavegameEditors.PokemonPicross={
@@ -9,7 +9,9 @@ SavegameEditors.PokemonPicross={
 
 	Offsets:{
 		PICRITES:0x0b40,
-		BOUGHTPICRITES:0x0b42,
+		BOUGHTPICRITES:0x0b44,
+		PENDINGPICRITES:0x0b48, /* if there is an error after buying, they are queued here */
+		UNLOCKEDSHOPFLAG:0x0b4c,
 	},
 
 	/* check if savegame is valid */
@@ -22,13 +24,16 @@ SavegameEditors.PokemonPicross={
 		tempFile.fileName='all.dat';
 
 		updateInput('picrites', tempFile.readShort(this.Offsets.PICRITES));
-		//updateInput('boughtpicrites', tempFile.readShort(this.Offsets.BOUGHTPICRITES));
+		updateInput('boughtpicrites', tempFile.readShort(this.Offsets.BOUGHTPICRITES));
 	},
 
 
 	/* save function */
 	save:function(){
 		tempFile.writeShort(this.Offsets.PICRITES, getInputNumber('picrites'));
-		//tempFile.writeShort(this.Offsets.BOUGHTPICRITES, getInputNumber('boughtpicrites'));
+		var boughtPicrites=getInputNumber('boughtpicrites');
+		tempFile.writeShort(this.Offsets.BOUGHTPICRITES, boughtPicrites);
+		var unlockedShopByte=tempFile.readByte(this.Offsets.UNLOCKEDSHOPFLAG) & ~0x01;
+		tempFile.writeByte(this.Offsets.UNLOCKEDSHOPFLAG, unlockedShopByte+(boughtPicrites>=5000)?0x01:0x00);
 	}
 }
