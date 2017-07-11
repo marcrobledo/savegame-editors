359586e798a2932fd110d565253c7e47fe4dec48
Marc Robledo <mark@tinet.org>
2017-07-11 22:39:21 +0200

    added kirbys blowout blast statue editor

---
+++ kirbys-blowout-blast/kirbys-blowout-blast.js
@@ -0,0 +1,40 @@
+/*
+	Kirby's Blowout Blast for HTML5 Save Editor v20160704
+	by Marc Robledo 2016
+*/
+
+SavegameEditor={
+	Name:'Kirby\'s Blowout Blast',
+	Filename:'savedata.dat',
+
+	StatuesOffset:0x07,
+
+	unlockAmiiboPuzzles:function(){
+		for(var i=0; i<5; i++)
+			tempFile.writeByte(this.StatuesOffset+i*2, 0x01);
+		tempFile.writeByte(this.StatuesOffset+1, 0x01);
+		setValue('amiibocount', 5);
+	},
+
+	/* check if savegame is valid */
+	checkValidSavegame:function(){
+		return (tempFile.fileSize==1736)
+	},
+
+	/* load function */
+	load:function(){
+		tempFile.fileName='savedata.dat';
+
+		var unlockedStatues=0;
+		for(var i=0; i<5; i++){
+			if(tempFile.readByte(this.StatuesOffset+i*2))
+				unlockedStatues++;
+		}
+		setValue('amiibocount', unlockedStatues);
+	},
+
+
+	/* save function */
+	save:function(){
+	}
+}
