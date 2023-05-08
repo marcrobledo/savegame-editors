9a92623bd3fb94fffb7f568766743455a3c046f6
Marc Robledo <mark@tinet.org>
2023-05-08 19:58:09 +0200

    improved the way the filenames are shown in the drag and drop container

--- savegame-editor.js
+++ savegame-editor.js
@@ -164,8 +164,15 @@ function getSavegameAllNames(){
 		return SavegameEditor.Filename;
 	else{
 		var s='';
-		for(var i=0; i<SavegameEditor.Filename.length; i++)
-			s+=SavegameEditor.Filename[i]+', ';
+		for(var i=0; i<SavegameEditor.Filename.length; i++){
+			if(i){
+				if(i===(SavegameEditor.Filename.length-1))
+					s+=' or ';
+				else
+					s+=', ';
+			}
+			s+=SavegameEditor.Filename[i];
+		}
 		return s
 	}
 }
