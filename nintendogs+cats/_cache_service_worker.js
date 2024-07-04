a15d739871bcb5aac4a62cf418baae8696d4c642
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-07-04 17:26:18 +0200

    Add Nintendogs + Cats item editor

---
+++ nintendogs+cats/_cache_service_worker.js
@@ -0,0 +1,82 @@
+/*
+	Cache Service Worker template by mrc 2019
+	mostly based in:
+	https://github.com/GoogleChrome/samples/blob/gh-pages/service-worker/basic/service-worker.js
+	https://github.com/chriscoyier/Simple-Offline-Site/blob/master/js/service-worker.js
+	https://gist.github.com/kosamari/7c5d1e8449b2fbc97d372675f16b566e	
+	
+	Note for GitHub Pages:
+	there can be an unexpected behaviour (cache not updating) when site is accessed from
+	https://user.github.io/repo/ (without index.html) in some browsers (Firefox)
+	use absolute paths if hosted in GitHub Pages in order to avoid it
+	also invoke sw with an absolute path:
+	navigator.serviceWorker.register('/repo/_cache_service_worker.js', {scope: '/repo/'})
+*/
+
+
+/* MOD: fix old caches for mrc */
+caches.keys().then(function(cacheNames){
+	for(var i=0; i<cacheNames.length; i++){
+		if(
+			cacheNames[i]==='runtime' ||
+			/^precache-\w+$/.test(cacheNames[i]) ||
+			/^precache-editor-([\w\+]+)-\w+$/.test(cacheNames[i]) ||
+			/^v?\d+\w?$/.test(cacheNames[i])
+		){
+			console.log('deleting old cache: '+cacheNames[i]);
+			caches.delete(cacheNames[i]);
+		}
+	}
+});
+
+var PRECACHE_ID='picross-3d-round-2-editor';
+var PRECACHE_VERSION='v1';
+var PRECACHE_URLS=[
+	'/savegame-editors/picross-3d-round-2/','/savegame-editors/picross-3d-round-2/index.html',
+	'/savegame-editors/picross-3d-round-2/favicon.png',
+	'/savegame-editors/picross-3d-round-2/picross-3d-round-2.js',
+	'/savegame-editors/savegame-editor.js',
+	'/savegame-editors/savegame-editor.css'
+];
+
+
+
+// install event (fired when sw is first installed): opens a new cache
+self.addEventListener('install', evt => {
+	evt.waitUntil(
+		caches.open('precache-'+PRECACHE_ID+'-'+PRECACHE_VERSION)
+			.then(cache => cache.addAll(PRECACHE_URLS))
+			.then(self.skipWaiting())
+	);
+});
+
+
+// activate event (fired when sw is has been successfully installed): cleans up old outdated caches
+self.addEventListener('activate', evt => {
+	evt.waitUntil(
+		caches.keys().then(cacheNames => {
+			return cacheNames.filter(cacheName => (cacheName.startsWith('precache-'+PRECACHE_ID+'-') && !cacheName.endsWith('-'+PRECACHE_VERSION)));
+		}).then(cachesToDelete => {
+			return Promise.all(cachesToDelete.map(cacheToDelete => {
+				console.log('delete '+cacheToDelete);
+				return caches.delete(cacheToDelete);
+			}));
+		}).then(() => self.clients.claim())
+	);
+});
+
+
+// fetch event (fired when requesting a resource): returns cached resource when possible
+self.addEventListener('fetch', evt => {
+	if(evt.request.url.startsWith(self.location.origin)){ //skip cross-origin requests
+		evt.respondWith(
+			caches.match(evt.request).then(cachedResource => {
+				if (cachedResource) {
+					return cachedResource;
+				}else{
+					return fetch(evt.request);
+				}
+			})
+		);
+	}
+});
---
+++ nintendogs+cats/nintendogs+cats.js
@@ -0,0 +1,83 @@
+/*
+	Nintendogs + Cats for HTML5 Save Editor v?
+	by Marc Robledo 2016
+*/
+var reg = /\d+/;
+SavegameEditor={
+	Name:'Nintendogs + Cats',
+	Filename:'sysdata.dat',
+
+	/* Constants */
+	Constants:{
+		MONEY_OFFSET:0xA0
+	},
+	
+	_write_money:function(){
+		tempFile.writeU32(
+			SavegameEditor.Constants.MONEY_OFFSET,
+			getValue('money')
+		);
+	},
+	_write_supply_amount:function(e){
+		tempFile.writeU8(
+			Number(e.target.dataset.offset),
+			getValue(e.target.id)
+		);
+	},
+	
+	/* check if savegame is valid */
+	checkValidSavegame:function(){
+		return (tempFile.fileSize==60936)
+	},
+	
+	preload:function() {
+		get('number-money').addEventListener('change', SavegameEditor._write_money);
+		
+		fetch('/savegame-editors/nintendogs+cats/supplies.json')
+			.then(function(response) {
+				return response.json();
+			}).then(function(data) {
+				var counter = 0;
+				for (var rowtype of [
+					['fooddrink', 'food & drink'],
+					['toys', 'toys'],
+					['accessories', 'accessories'],
+					['furniture', 'furnitures'],
+					['leashes', 'leashes'],
+					['skins', 'skins']
+				]){
+					var rt = get('row-'+rowtype[0]);
+					counter = 0;
+					for (var entry of data[rowtype[1]]) {
+						rt.append(
+							col(3, span(entry[1]+' ('+entry[0]+')')),
+							col(1, inputNumber('supplies_' + rowtype[0] + '_'+counter+'_amount', 0, 99, tempFile.readU8(Number(entry[0]))))
+						);
+						get('number-supplies_' + rowtype[0] + '_'+counter+'_amount').dataset.offset = entry[0];
+						get('number-supplies_' + rowtype[0] + '_'+counter+'_amount').addEventListener('change', SavegameEditor._write_supply_amount);
+						counter++;
+					}
+					var lastRow = counter%3;
+					if (lastRow !== 0) {
+						rt.append(col((3-lastRow) * 4, span('')));
+					}
+				}
+			}).catch(function(error) {
+				console.log('[Picross Save Editor]', error);
+			});
+	},
+
+	/* load function */
+	load:function(){
+		tempFile.fileName='sysdata.dat';
+		tempFile.littleEndian=true;
+
+		setValue('money', tempFile.readU32(SavegameEditor.Constants.MONEY_OFFSET));
+		setNumericRange('money', 0, 9999999);
+	},
+
+
+	/* save function */
+	save:function(){
+	}
+}
